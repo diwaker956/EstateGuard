624b8c70813749a384c6c8a270729e78d8fddfff
risu9892 <risusingh226@gmail.com>
2024-03-17 20:33:18 +0530

    first commit

---
+++ backend/src/main/java/com/property/estateGuard/EstateGuardApplication.java
@@ -0,0 +1,13 @@
+package com.property.estateGuard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstateGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstateGuardApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/property/estateGuard/EstateGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.property.estateGuard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstateGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
